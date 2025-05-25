572c591e3d002c4fedf71c9a61086b44c98035ba
Septiana Trikuncoro <kurokunc@gmail.com>
2025-05-25 23:56:47 +0700

    :sparkles: setup tailwind / daisyui / iconify

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
+  <button class="btn btn-soft btn-secondary">
+    Click Icon <Icon name="tabler:map" size="24px" />
+  </button>
 </template>
