e95cdf805809556739887e192bbdb33d54c7da07
Septiana Trikuncoro <kurokunc@gmail.com>
2025-05-26 01:00:17 +0700

    :sparkles: add landing page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NuxtLink } from "#components";
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In With Github
+        <Icon name="tabler:brand-github" size="24px" />
+      </button>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with this simple travel log app. Add locations, photos, and notes to create a digital journal of your journeys.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24px" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
