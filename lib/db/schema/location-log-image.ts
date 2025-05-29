39f3de84f7a851deeb55c2327c5a35553a72275d
Septiana Trikuncoro <kurokunc@gmail.com>
2025-05-29 14:47:44 +0700

    :sparkles: auth setup

--- lib/db/schema/location-log-image.ts
+++ lib/db/schema/location-log-image.ts
@@ -4,14 +4,14 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 // import { createInsertSchema } from "drizzle-zod";
 
-// import { user } from "./auth";
+import { user } from "./auth";
 import { locationLog } from "./location-log";
 
 export const locationLogImage = sqliteTable("locationLogImage", {
   id: int().primaryKey({ autoIncrement: true }),
   key: text().notNull(),
   locationLogId: int().notNull().references(() => locationLog.id, { onDelete: "cascade" }),
-  // userId: int().notNull().references(() => user.id, { onDelete: "cascade" }),
+  userId: int().notNull().references(() => user.id, { onDelete: "cascade" }),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
--- lib/env.ts
+++ lib/env.ts
@@ -6,6 +6,16 @@ const EnvSchema = z.object({
   NODE_ENV: z.string(),
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
+  // AUTH_GITHUB_CLIENT_ID: z.string(),
+  // AUTH_GITHUB_CLIENT_SECRET: z.string(),
+  // S3_ENDPOINT: z.string(),
+  // S3_ACCESS_KEY: z.string(),
+  // S3_ACCESS_SECRET: z.string(),
+  // S3_REGION: z.string(),
+  // S3_BUCKET: z.string(),
+  // S3_BUCKET_URL: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
